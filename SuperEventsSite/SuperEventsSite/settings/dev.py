eec8d17ccc4c7c32a1228e4bcf79e411d1eadc2b
Denis Grigoriev <denisgrigoriev04@ya.ru>
2023-10-14 12:30:16 +0300

    Создан проект wagtail Первая модель успешно изменена

---
+++ SuperEventsSite/SuperEventsSite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-2_=pcfi0)71k%hxentz-rf3k=#xz91_s4+by3i*%udq2%)yxvt"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
