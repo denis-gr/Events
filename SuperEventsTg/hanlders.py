287bf83aaca7257f3076f37fabc67f1b8f3163a9
Denis Grigoriev <denisgrigoriev04@ya.ru>
2023-10-14 18:06:09 +0300

    Теперь есть QR для всего Теперь можно сразу переходить к нужному меропритию и др

--- SuperEventsTg/hanlders.py
+++ SuperEventsTg/hanlders.py
@@ -9,8 +9,17 @@ from keyboards import get_choise_event_keyboard, ChoiseEventCallbackFactory, get
 dp = aiogram.Router()
 
 @dp.message(aiogram.filters.Command("start"))
-async def cmd_start(mes: aiogram.types.Message, command: aiogram.filters.CommandObject):
+async def cmd_start(mes: aiogram.types.Message,
+command: aiogram.filters.CommandObject, db: DB
+):
     await mes.answer(_("start_mes"))
+    typepage, id, extra = (command.args or "none-none").split("-")
+    await {
+        "event": choise_event_m,
+        "hall": choise_hall_m,
+        "performance": choise_pref_m,
+        "none": lambda *args,  **kwargs: None
+    }[typepage](mes, db=db, id=id)
 
 
 @dp.message(aiogram.filters.Command("get_public_events"))
@@ -29,7 +38,7 @@ command: aiogram.filters.CommandObject, db: DB
 
 
 @dp.callback_query(ChoiseEventCallbackFactory.filter())
-async def choise_event(callback: aiogram.types.CallbackQuery, 
+async def choise_event_cq(callback: aiogram.types.CallbackQuery, 
 db: DB, callback_data: ChoiseEventCallbackFactory):
     event = db.getEvent(callback_data.id)
     halls = db.getHallsFromEvent({ "path": [callback_data.path] })
@@ -39,8 +48,17 @@ db: DB, callback_data: ChoiseEventCallbackFactory):
     await callback.answer()
 
 
+async def choise_event_m(mes: aiogram.types.Message, 
+db: DB, id: int):
+    event = db.getEvent(id)
+    halls = db.getHallsFromEvent(event)
+    halls = halls.iterrows()
+    halls = list(halls)
+    await mes.answer(_("event", event=event), reply_markup=get_choise_event_hall_keyboard(halls))
+
+
 @dp.callback_query(ChoiseEventHallCallbackFactory.filter())
-async def choise_event(callback: aiogram.types.CallbackQuery, 
+async def choise_hall_cq(callback: aiogram.types.CallbackQuery, 
 db: DB, callback_data: ChoiseEventHallCallbackFactory):
     hall = db.getHall(callback_data.id)
     perfs = db.getPerformancesFromHall({ "path": [callback_data.path] })
@@ -50,11 +68,26 @@ db: DB, callback_data: ChoiseEventHallCallbackFactory):
     await callback.answer()
 
 
+async def choise_hall_m(mes: aiogram.types.Message, 
+db: DB, id: int):
+    hall = db.getHall(id)
+    perfs = db.getPerformancesFromHall(hall)
+    perfs = perfs.iterrows()
+    perfs = list(perfs)
+    await mes.answer(_("hall", hall=hall), reply_markup=get_choise_hall_perfs_keyboard(perfs))
+
+
 @dp.callback_query(ChoiseHallPerfsCallbackFactory.filter())
-async def choise_event(callback: aiogram.types.CallbackQuery, 
+async def choise_pref(callback: aiogram.types.CallbackQuery, 
 db: DB, callback_data: ChoiseHallPerfsCallbackFactory):
     pref = db.getPerformances(callback_data.id)
     await callback.message.edit_text(_("pref", pref=pref), reply_markup=None) # TODO
     await callback.answer()  
+
+
+async def choise_pref_m(mes: aiogram.types.Message, 
+db: DB, id: int):
+    pref = db.getPerformances(id)
+    await mes.answer(_("pref", pref=pref), reply_markup=None) #TODO
 
 
